cef6c5ed0b3fe3851af3e8682a10aa9e8932fbd5
Samuel Odewale <edusegzy2@gmail.com>
2019-05-04 13:08:38 -0500

    App ready to deploy

---
+++ app.py
@@ -0,0 +1,48 @@
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.jinja2')
+
+@app.route('/divide_form')
+def divide_form():
+    return render_template('divide_form.jinja2')
+
+@app.route('/divide_result', methods=['POST'])
+def divide_result():
+    number = float(request.form['number'])
+    divide_by = float(request.form['divide_by'])
+    return render_template('divide_result.jinja2', result=(number*0.453592)/(divide_by*0.3048)**2)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+"""
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.jinja2')
+
+@app.route('/divide', methods=['GET', 'POST'])
+def divide():
+    result = None
+    if request.method == 'POST':
+        number = float(request.form['number'])
+        divide_by = float(request.form['divide_by'])
+        result = number / divide_by
+    return render_template('divide.jinja2', result=result)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+    """
